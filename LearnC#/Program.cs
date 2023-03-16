4b7cf0e66e6e0101a44d552f96fe6533ec009579
kekelberi <alexei.gaponov@gmail.com>
2023-03-15 21:21:50 -0500

    Добавьте файлы проекта.

---
+++ LearnC#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string da = "da";
+            Console.WriteLine(da);
+            Console.ReadLine();
+        }
+    }
+}
