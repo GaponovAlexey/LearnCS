d02ba2915d10c84d32ba631be0cd39eaab06b8d8
kekelberi <alexei.gaponov@gmail.com>
2023-03-19 21:26:38 -0500

    save

--- LearnC#/Program.cs
+++ LearnC#/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
 namespace LearnC_
@@ -8,61 +10,10 @@ namespace LearnC_
     {
         static void Main(string[] args)
         {
-            Performer work1 = new Performer("Ivan");
-            Performer work2 = new Performer("Petro");
-
-            Task[] task = { new Task(work1, "Vikopat yamu"), new Task(work2, "Postroit dom") };
-
-            Boar sch = new Boar(task);
-            sch.ShowAllTasks();
 
             Console.ReadLine();
         }
-
-
     }
-    class Performer
-    {
-        public string Name;
-        public Performer(string name)
-        {
-            Name = name;
-        }
-
-    }
-    class Boar
-    {
-        public Task[] Tasks;
-        public Boar(Task[] tasks)
-        {
-            Tasks = tasks;
-        }
-        public void ShowAllTasks()
-        {
-            for (int i = 0; i < Tasks.Length; i++)
-            {
-                Tasks[i].ShowInfo();
-
-            }
-        }
-    }
-
-    class Task
-    {
-        public Performer Worker;
-        public string Description;
-        public Task(Performer worker, string description)
-        {
-            Worker = worker;
-            Description = description;
-        }
-
-        public void ShowInfo()
-        {
-            Console.WriteLine($"otvetsven: {this.Worker.Name}\nOpicanie Zada4i: {this.Description}");
-        }
-    }
-
 }
 
 
